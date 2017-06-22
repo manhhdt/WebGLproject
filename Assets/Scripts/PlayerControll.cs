dd971ad4718e4892e96d968ead184f9b6bd69f11
VDT\ASUS <vdt.uit.studio@gmail.com>
2017-06-22 12:17:29 +0700

    Jump up and score

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -1,33 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class PlayerControll : MonoBehaviour {
 
 	public float speed=6.0F;
-
+	public float speed2=10.0F;
 	private Rigidbody rb;
-
+	private bool Jumping = false;
+	private int score = 0;
+	public Text scoreText;
+	private Vector3 originalPosition ;
+	private Quaternion originalRotation;
+	public Text timerText;
+	private float secondsCount;
+	private bool isOverGame = false;
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		scoreText.text = "0";
+		originalPosition = transform.position;
+		originalRotation = transform.rotation;
 	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		if (!isOverGame) {
+			
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+			//Debug.Log ("Hrizontal " + moveHorizontal);
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				rb.velocity = Vector3.up * speed2;
+			}
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
 
-		rb.AddForce (movement * speed);
+			if (transform.position.y < 0) {
+				Debug.Log ("Game over");
+				transform.position = originalPosition;
+				transform.rotation = originalRotation;
+				if (rb != null) {
+					rb.velocity = Vector3.zero;
+					rb.angularVelocity = Vector3.zero;
+				}
+				//set timer UI
+				timerText.text = "Game over " + (int)secondsCount + " s";
+				isOverGame = true;
+			} else {
+				if (score < 210) {
+					UpdateTimerUI ();
+				}
+			}
+
+			if (score == 210) {
+				timerText.text = "Complete with " + (int)secondsCount + " s";
+			}
+		} else {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				isOverGame = false;
+				secondsCount = 0.0f;
+				SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+			}
+		}
 	}
 
+	//call this on update
+	public void UpdateTimerUI(){
+		//set timer UI
+		secondsCount += Time.deltaTime;
+		timerText.text = "Timer " + (int)secondsCount + " s";
+	 
+	}
 	void OnTriggerEnter(Collider other) 
 	{
 		if (other.gameObject.CompareTag ("PickUp"))
 		{
 			other.gameObject.SetActive (false);
+			score+=10;
+			scoreText.text = score.ToString ();
 		}
 	}
 }
